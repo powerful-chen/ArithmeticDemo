05a89798930687cdcab7eb2dd516234b59a94a5f
powerful-chen <2930763626@qq.com>
2021-09-19 20:41:15 +0800

    first commit

---
+++ arithmetic/src/Array/MaoPao.java
@@ -0,0 +1,26 @@
+package Array;
+
+import java.util.Arrays;
+
+//√∞≈›≈≈–ÚÀ„∑®
+public class MaoPao {
+
+	public static void main(String[] args) {
+		maopao();
+	}
+
+	public static void maopao() {
+		int[] arr = { 7, 3, 12, 1, 0 };
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
